e88616f8b61771b9c8a8431e0fe4f5559d1d6a3f
Nusrat Jahan <154794781+jlk776ryti6r76@users.noreply.github.com>
2025-09-02 21:40:33 +0600

    Delete

--- hospital-management-system-main/includes/footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-    <div class="container">
-        <p>© <?php echo date("Y"); ?> City Hospital. All Rights Reserved.</p>
-        <p>Contact: contact@cityhospital.com | +1 234 567 890</p>
-    </div>
-</footer>
-</body>
-</html>
--- hospital-management-system-main/index.php
+++
@@ -1,3 +0,0 @@
-<?php include "includes/navbar.php"; ?>
-
-<h1 style="text-align:center; margin-top:50px;">Welcome to City Hospital</h1>
